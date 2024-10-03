1e60b11e210fb07f1b76c5f4c2cd2a6a6f161617
Meepyster <iforlemu2876@gmail.com>
2024-10-03 19:27:18 -0400

    idk

--- Assets/Ability Data/Ability.cs
+++ Assets/Ability Data/Ability.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Ability : MonoBehaviour
-
+public class Ability : MonoBehaviour {
     public Ability (AbilityName ability, Character[] allyList, Character[] enemyList, int castIndex, int targetIndex){
         // This will then call the required ability from searching through each case
         // With the enum
